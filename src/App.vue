1ac22cf06a7ece7f1e93eb6e7f47b43b9d931b5a
AnnaYureva <a.yureva@worksolutions.ru>
2024-09-17 12:40:55 +0300

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <Header />
-  <RouterView />
+  <div class="content">
+    <div>
+      <Header />
+      <main>
+        <RouterView />
+      </main>
+    </div>
+    <Footer />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.content {
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: 1fr auto;
+
+  & main {
+    max-width: 616px;
+    width: 100%;
+    margin: 30px auto;
+  }
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        <p>
+            all rights reserved
+        </p>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+@import '/src/assets/variables.scss';
+
+    footer {
+        background-color: $gray-m;
+        color: $black;
+        padding: 30px;
+        text-align: center;
+    }
+</style>
