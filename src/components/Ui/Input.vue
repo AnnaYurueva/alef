3c4d4e40defd224c5ab0169e3157f4ec9321cd0c
AnnaYureva <a.yureva@worksolutions.ru>
2024-09-17 13:05:09 +0300

    create input component

---
+++ src/components/Ui/Input.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="input">
+        <label for="input">{{ label }}</label>
+        <input type="text" v-model="inputData" />
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    label: {
+        type: String,
+        required: true
+    }
+})
+const inputData = defineModel('inputData')
+</script>
+
+<style scoped lang="scss">
+@import '/src/assets/variables.scss';
+
+.input {
+    display: grid;
+    padding: 7px 16px;
+    border-radius: 4px;
+    border: 1px solid $gray-l;
+
+    & label {
+        font-size: 13px;
+        color: $gray;
+    }
+
+    & input {
+        border: none;
+
+        &:focus {
+            outline: none;
+
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,8 @@ import Button from '@/components/Ui/Button.vue'
 
 <template>
   <main>
-    <h1>Персональные данные</h1>
-    <h2>Дети (макс. 5)</h2>
+    <h1 class="title">Персональные данные</h1>
+    <h2 class="title">Дети (макс. 5)</h2>
 
     <Button>Сохранить</Button>
     <Button variant="outline">Добавить ребенка</Button>
